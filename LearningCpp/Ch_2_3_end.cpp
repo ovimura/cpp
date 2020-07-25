16acb0def85f34bf29d1d97ef2325fa24935c30f
Ovidiu Mura <mura@pdx.edu>
2020-07-25 13:28:23 -0700

    adding new file with randomized chip pile

---
+++ LearningCpp/Ch_2_3_end.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	bool player1Turn = true;
+	bool gameOver = false;
+	
+	int chipsInPile = 0;
+	int chipsTaken = 0;
+	
+	srand(time(0));
+
+	chipsInPile = (rand() % 100) + 1;
+	cout << "This round will start with " << chipsInPile << " chips in the pile\n";
+	
+	return 0;
+}
+
