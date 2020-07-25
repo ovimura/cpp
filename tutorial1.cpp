030f0cb3a183ca1bcfbde7847016600cf7d65730
Ovidiu Mura <mura@pdx.edu>
2020-07-25 13:13:42 -0700

    adding tutorial c++ files of the course Learning C++

---
+++ tutorial1.cpp
@@ -0,0 +1,18 @@
+// Hello World
+/*
+ * By Erin Colvin
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Please enter your name:" << endl;
+	cin >> name;
+	cout << "hello, " << name << endl;
+	return 0;
+}
+
---
+++ tutorial2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string response;
+	cout << "What's your favorite food?" << endl;
+	cin >> response;
+	cout << "My favorite food is " << response << "." << endl;
+	
+	return 0;
+}
+
+
